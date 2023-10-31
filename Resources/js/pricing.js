401feb129ddd612ed94b1f828e88d48b41932106
PabloGabrielDonato <iphone.pablo2002@gmail.com>
2023-10-30 21:56:44 -0300

    todo ok

---
+++ Resources/js/pricing.js
@@ -0,0 +1,26 @@
+const data = [
+  {
+    id: 1,
+    title: "Front End",
+    description:
+      "We will do all the front end development, including the website design, its functionality using HTML5, CSS, JS, frameworks like Bootstrap 5, VUE and AXIOS and technologies like Photoshop. We make the websites full responsive. In English or Spanish",
+    price: "",
+    image_url: "",
+  },
+  {
+    id: 2,
+    title: "Back End",
+    description:
+      "We will do all the front end development, including the website design, its functionality using HTML5, CSS, JS, frameworks like Bootstrap 5, VUE and AXIOS and technologies like Photoshop. We make the websites full responsive. In English or Spanish",
+    price: "",
+    image_url: "",
+  },
+  {
+    id: 3,
+    title: "Fullstack",
+    description:
+      "We will do all the front end development, including the website design, its functionality using HTML5, CSS, JS, frameworks like Bootstrap 5, VUE and AXIOS and technologies like Photoshop. We make the websites full responsive. In English or Spanish",
+    price: "",
+    image_url: "",
+  },
+];
