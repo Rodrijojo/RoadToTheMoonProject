dfd9f4ec0bde40f319c18cdcac95445d63f28e05
PabloGabrielDonato <iphone.pablo2002@gmail.com>
2023-10-31 01:10:23 -0300

    estilo del form

--- enviar_formulario.php
+++ enviar_formulario.php
@@ -1,37 +1,29 @@
 <?php
-$enviaPara = 'iphone.pablo2002@gmail.com';
+$enviaPara = 'iphone.pablo2002@gmail.com'; // Cambia 'correo_destino@example.com' por la dirección de correo a la que deseas enviar los formularios.
 $subject = 'Contacto desde la web';
-$enviado = "enviado.html";
+$enviado = "enviado.html"; // Asegúrate de que este sea el archivo correcto donde deseas redirigir después de enviar el formulario.
 
 $mensaje = '';
 $primero = true;
 
-// Definir un array que mapee los nombres de campo del formulario a etiquetas humanas
 $campos = array(
     'nombre' => 'Nombre',
+    'apellido' => 'Apellido',
     'email' => 'Email',
     'telefono' => 'Teléfono',
-    'motivo' => 'Motivo de Contacto'
+    'mensaje' => 'Mensaje'
 );
 
 foreach ($_POST as $campo => $valor) {
     if (isset($campos[$campo])) {
-        if (is_array($valor)) {
-            $mensaje .= '<strong>' . $campos[$campo] . ':</strong><ul>';
-            foreach ($valor as $item) {
-                $mensaje .= '<li>' . $item . '</li>';
-            }
-            $mensaje .= '</ul><br>';
-        } else {
-            if ($primero) {
-                $from = $valor;
-                $primero = false;
-            }
-            $mensaje .= '<strong>' . $campos[$campo] . ':</strong>';
-            $mensaje .= $valor . '<br>';
-            if (strpos($valor, '@') > 3 && strpos($valor, '.') > -1) {
-                $from = $valor;
-            }
+        if ($primero) {
+            $from = $valor;
+            $primero = false;
+        }
+        $mensaje .= '<strong>' . $campos[$campo] . ':</strong>';
+        $mensaje .= $valor . '<br>';
+        if (filter_var($valor, FILTER_VALIDATE_EMAIL)) {
+            $from = $valor;
         }
     }
 }
@@ -39,6 +31,9 @@ foreach ($_POST as $campo => $valor) {
 $mail_headers = "MIME-Version: 1.0\r\n";
 $mail_headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
 $mail_headers .= 'From: ' . $from . "\r\n";
-@mail($enviaPara, $subject, $mensaje, $mail_headers);
-header("Location: $enviado");
+
+if (isset($_POST['submit'])) {
+    @mail($enviaPara, $subject, $mensaje, $mail_headers);
+    header("Location: $enviado");
+}
 ?>
